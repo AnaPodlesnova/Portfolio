73e36beb095632d539da99e69492e9161b6fe90b
AnaPodlesnova <anet258704@gmail.com>
2026-07-22 02:03:01 +0300

    v-2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: ["babel-plugin-styled-components"],
+//       },
+//     }),
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": "/src",
+//     },
+//   }
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({
+
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/repo-name/" : "/",
   plugins: [
     react({
       babel: {
@@ -12,5 +31,5 @@ export default defineConfig({
     alias: {
       "@": "/src",
     },
-  }
-});
+  },
+}));
